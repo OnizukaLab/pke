81de924c0c22029dd0bf820557880327f91d9a23
HigashiKed <eastkin0815@gmail.com>
2019-12-20 02:52:20 +0000

    fasttext

--- pke/__init__.py
+++ pke/__init__.py
@@ -6,6 +6,7 @@ from pke.base import LoadFile
 from pke.utils import (load_document_frequency_file, compute_document_frequency,
                        train_supervised_model, load_references,
                        compute_lda_model, load_document_as_bos,
-                       compute_pairwise_similarity_matrix)
+                       compute_pairwise_similarity_matrix,
+                       load_fasttext_model)
 import pke.unsupervised
 import pke.supervised
